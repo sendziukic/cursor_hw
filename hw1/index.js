4c93dda1dca15d7f07400f017c62416c3f76a376
Roman_Sendziuk <sendzyuk.r@gmail.com>
2020-07-25 08:39:50 +0300

    last update for adv task

--- hw1/index.js
+++ hw1/index.js
@@ -2,30 +2,30 @@ const bikePrice = 15.678;
 const carPrice = 90.2345;
 const truckPrice = 123.965;
 
-//Використовуючи вбудований об'єкт Math – виведіть максимальне число
+//
 const maxPrice = Math.max(bikePrice, carPrice, truckPrice);
 document.writeln(`${maxPrice}<hr>`);
 
-//Використовуючи вбудований об'єкт Math – виведіть мінімальне число
+//
 const minPrice = Math.min(bikePrice, carPrice, truckPrice);
 document.writeln(`${minPrice}<hr>`);
 
-//Складіть вартість всіх товарів, помістіть її в змінну та виведіть цю суму
+//
 const allGoods = bikePrice + carPrice + truckPrice
 const summPrice = allGoods;
 document.writeln(`${summPrice}<hr>`);
 
-//Відкиньте копійки у всіх товарів :), потім – складіть цілу частину вартості кожного товару між собою.
-//Округлення використовувати в МЕНЬШУ сторону.
+//
+//
 
 const summInt = Math.floor(bikePrice) + Math.floor(carPrice) + Math.floor(truckPrice);
 document.writeln(`${summInt}<hr>`);
 
-//Виведіть суму товарів округлену до сотень. (Наприклад якщо вийшло 260, то виведіть 300)
+//
 const roundedSumm = Math.round(summPrice / 100) * 100
 document.writeln(`${roundedSumm}<hr>`);
 
-//Виведіть булеве значення: чи є сума всіх товарів (округлена в меншу сторону) парним чи непарним числом?
+//
 let message = "";
 const isEven = summInt % 2 == 0;
 if (isEven) {
@@ -36,11 +36,11 @@ else {
 }
 document.writeln(message);
 
-//Виведіть суму решти, при оплаті всіх товарів (без округлення), якщо клієнт платить 500 грн.
+//
 const changeFrom500 = 500 - summPrice;
 document.writeln(`${changeFrom500}<hr>`);
 
-//Виведіть середнє значення цін, округлене до другого знаку після коми
+//
 const average = (summPrice / [bikePrice.toFixed, carPrice.toFixed, truckPrice].length).toFixed(2);
 document.writeln(`${average}<hr>`);
 
@@ -57,5 +57,12 @@ document.writeln(`price: ${trainPrice}$, discount: ${discount * 100} %, new pric
 
 
 document.writeln(`<h3>Advanced Task</h3> 
-${summPrice}`)
+Max price: ${summPrice};<br>
+Min price: ${minPrice};<br>
+All goods price: ${summPrice};<br>
+Sum of rounded integers: ${summInt};<br>
+Sum, rounded to low. 100: ${roundedSumm};<br>
+Sum of rounded integers is ${message}
+Change for 500: ${changeFrom500};<br>
+Average price: ${average};<br>`)
 
